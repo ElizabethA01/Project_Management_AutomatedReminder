d68597aacfa37ce680a84e280851cd5b23c5a0ff
Elizabeth <a.adejumo13@gmail.com>
2022-12-13 17:10:11 +0000

    add default to variable spreadsheet work

--- Modules/disciplines_details.py
+++ Modules/disciplines_details.py
@@ -1,19 +1,12 @@
 # define class for discipline leads
 class MDLTracker:
-    # start_row = None
-    # end_row = None
     def __init__(self, start_row: int, end_row: int) -> None:
         self.start_row = start_row
         self.end_row = end_row
 
 class DisciplineLead:
-    # first_name = None
-    # last_name = None
-    # email= None
-    # discipline= None
-    spreadsheet_work= MDLTracker
 
-    def __init__(self, first_name: str, last_name: str, email: str, discipline: str, spreadsheet_work: MDLTracker) -> None:
+    def __init__(self, first_name: str, last_name: str, email: str, discipline: str, spreadsheet_work: MDLTracker = None) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
