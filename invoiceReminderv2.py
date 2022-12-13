2d0fa7a91745680bfb595c43b46d73f4ae393109
Elizabeth <a.adejumo13@gmail.com>
2022-12-13 20:18:41 +0000

    updated variables at the start of the main file and changed to dictionary to make it more readable

--- invoiceReminderv2.py
+++ invoiceReminderv2.py
@@ -5,11 +5,15 @@ from Modules.ExcelTracker import ExcelTracker
 from Modules.disciplines_details import DisciplineLead
 
 # email tracking list excel file location
-email_tracker_filename = r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\Email_listing_tracker.xlsx'
-sheetname = 'Invoice'
-contacts_list_filename = r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\List_of_contacts.xlsx'
-mailCC_contacts = 'carolina.morales@wsp.com'
-required_contacts = ['David', 'Benny', 'Nelson']
+filenames = {
+    "email_tracker": r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\Email_listing_tracker.xlsx',
+    "sheetname": 'Invoice',
+    "contacts_list": r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\List_of_contacts.xlsx',
+}
+contacts= {  
+    "mailCC": 'carolina.morales@wsp.com', # add correct email address
+    "mailTo": ['David', 'Benny', 'Nelson']
+}
 
 def send_invoice_email(tracker: str, sheetname: str, contact_list: str, required_contacts: str, cc_contacts: str):
     for lead in ExcelAdapter(contact_list).df_to_list():
@@ -21,7 +25,7 @@ def send_invoice_email(tracker: str, sheetname: str, contact_list: str, required
                 ExcelTracker(tracker, sheetname).add_to_tracker(first_name= leads.first_name, last_name=leads.last_name, discipline=leads.discipline, email= leads.email, cc_contacts=cc_contacts)
 
 if __name__ == "__main__":
-    send_invoice_email(email_tracker_filename, sheetname, contacts_list_filename, required_contacts, mailCC_contacts)
+    send_invoice_email(filenames['email_tracker'], filenames['sheetname'], filenames['contacts_list'], contacts['mailTo'], contacts['mailCC'])
 
 
 
