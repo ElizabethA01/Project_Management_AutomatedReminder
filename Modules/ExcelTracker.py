4b95c65099917a7f5a79223d40c0e09ccd704eed
Elizabeth <a.adejumo13@gmail.com>
2022-12-13 19:04:47 +0000

    new emails is shown in the email tracker correctly

--- Modules/ExcelTracker.py
+++ Modules/ExcelTracker.py
@@ -1,28 +1,23 @@
 import openpyxl
 import pandas as pd
-from DraftEmail import *
+from DraftEmail import TimeStamp, SendEmail
 
 class ExcelTracker:
-    wb = openpyxl.load_workbook(email_tracker_filename)
-    email_tracker_filename = r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\Email_listing_tracker.xlsx'
-    sheetname = None
-    alert = None
-
     def __init__(self, email_tracker_filename: str, sheetname: str, alert: str =None) -> None:
         self.email_tracker_filename = email_tracker_filename
         self.sheetname = sheetname
-        self.alert = alert
-        
+        self.alert = alert     
 
     def append_to_sheet(self, first_name, last_name, discipline, email):
+        wb = openpyxl.load_workbook(self.email_tracker_filename)
         if self.sheetname == "Timesheet":
-            df = pd.DataFrame([[TimeStamp.now, first_name, last_name, discipline, email, SendEmail.mailCC_contacts, self.sheetname + ' - '+ self.alert]])
+            df = pd.DataFrame([[TimeStamp.now, first_name + ' ' + last_name, discipline, email, SendEmail.mailCC_contacts, self.sheetname + ' - '+ self.alert]])
         else: 
-            df = pd.DataFrame([[TimeStamp.now, first_name, last_name, discipline, email, SendEmail.mailCC_contacts, self.sheetname]])
+            df = pd.DataFrame([[TimeStamp.now, first_name + ' ' + last_name, discipline, email, SendEmail.mailCC_contacts, self.sheetname]])
         writer = pd.ExcelWriter(self.email_tracker_filename, if_sheet_exists='overlay', mode='a', engine='openpyxl')
-        ExcelTracker.wb.active = ExcelTracker.wb[self.sheetname]
+        wb.active = wb[self.sheetname]
         # get the max rows of non-empty cells
-        filled_rows = len([row for row in ExcelTracker.wb.active if not all([cell.value is None for cell in row])])
+        filled_rows = len([row for row in wb.active if not all([cell.value is None for cell in row])])
         # #Convert dataframe to an Xlsxwrite excel object
         df.to_excel(writer, sheet_name= self.sheetname, index=False, header=False, startrow=filled_rows)
         writer.close()
@@ -30,7 +25,8 @@ class ExcelTracker:
         
     # if sheet does not exist
     def create_new_sheet(self):
-        ExcelTracker.wb.create_sheet(self.sheetname)
+        wb = openpyxl.load_workbook(self.email_tracker_filename)
+        wb.create_sheet(self.sheetname)
         df_head = pd.DataFrame([['DATETIME', 'SENT TO', 'DISCIPLINE', 'EMAIL ADDRESS: TO', 'EMAIL ADDRESS: CC', 'TYPE']])
         writer = pd.ExcelWriter(self.email_tracker_filename, if_sheet_exists='overlay', mode='a', engine='openpyxl')
         df_head.to_excel(writer, sheet_name= self.sheetname, index=False, header=False)
@@ -38,14 +34,15 @@ class ExcelTracker:
 
     def create_new_file(self):
         # create a new file with headings
-        ExcelTracker.wb = openpyxl.Workbook()
-        ExcelTracker.wb.save(self.email_tracker_filename)
+        wb = openpyxl.Workbook()
+        wb.save(self.email_tracker_filename)
         print('new file created')
 
     # check if file does exist 
     def add_to_tracker(self, first_name: str, last_name: str, discipline: str, email: str):
+        wb = openpyxl.load_workbook(self.email_tracker_filename)
         try:
-            if self.sheetname in ExcelTracker.wb.sheetnames: 
+            if self.sheetname in wb.sheetnames: 
                 # if file exists, new email log is appended to file
                 self.append_to_sheet(first_name, last_name, discipline, email)
             else:
@@ -56,3 +53,6 @@ class ExcelTracker:
             self.create_new_file()
             self.create_new_sheet()
             self.append_to_sheet(first_name, last_name, discipline, email)
+
+
+
--- invoiceReminderv2.py
+++ invoiceReminderv2.py
@@ -2,23 +2,25 @@
 from Modules.DraftEmail import InvoiceEmail
 from Modules.ExcelAdapter import ExcelAdapter
 from Modules.ExcelTracker import ExcelTracker
-from Modules.discipline_detail import DisciplineLead, MDLTracker
+from Modules.disciplines_details import DisciplineLead
 
 # email tracking list excel file location
 email_tracker_filename = r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\Email_listing_tracker.xlsx'
-invoice_sheetname = 'Invoice'
-contacts_list_filename = r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\List_of_contacts.xlsx'
-required_contacts = ['David', 'Benny', 'Nelson']
+sheetname = 'Invoice'
+contacts_list_filename = r'C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\List_of_contacts.xlsx'
+# required_contacts = ['David', 'Benny', 'Nelson']
+required_contacts = ['Benny']
 
-def send_invoice_email():
-    for lead in ExcelAdapter(contacts_list_filename).df_to_list():
-        leads = DisciplineLead(first_name=lead[0], last_name=lead[1], email=lead[2], discipline=lead[3], spreadsheet_work=MDLTracker(start_row=lead[4], end_row=lead[5]))
+def send_invoice_email(tracker, sheetname, contacts):
+    for lead in ExcelAdapter(contacts).df_to_list():
+        leads = DisciplineLead(first_name=lead[0], last_name=lead[1], email=lead[2], discipline=lead[3])
         if leads.first_name in required_contacts:
             InvoiceEmail.send_invoice_reminder(first_name= leads.first_name, discipline=leads.discipline, email_to= leads.email)
-            ExcelTracker.add_to_tracker()
+            ExcelTracker(tracker, sheetname).add_to_tracker(first_name= leads.first_name, last_name=leads.last_name, discipline=leads.discipline, email= leads.email)
 
 
-send_invoice_email()
+if __name__ == "__main__":
+    send_invoice_email(email_tracker_filename, sheetname, required_contacts)
 
 
 
