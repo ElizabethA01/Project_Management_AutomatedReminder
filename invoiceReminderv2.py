c18c08f2d285bc4931035597bc1a65d0f0d9455d
Elizabeth <a.adejumo13@gmail.com>
2022-12-15 15:16:06 +0000

    modify invoice reminder python file to print error if email format is incorrect

--- invoiceReminderv2.py
+++ invoiceReminderv2.py
@@ -23,6 +23,8 @@ def send_invoice_email(tracker: str, sheetname: str, contact_list: str, required
             if outcome is True:
                 print('Invoice email alert activated - message sent')
                 ExcelTracker(tracker, sheetname).add_to_tracker(first_name= leads.first_name, last_name=leads.last_name, discipline=leads.discipline, email= leads.email, comment=sheetname, cc_contacts=cc_contacts, )
+            else:
+                print(outcome)
 
 if __name__ == "__main__":
     send_invoice_email(filenames['email_tracker'], filenames['sheetname'], filenames['contacts_list'], contacts['mailTo'], contacts['mailCC'])
--- timesheetReminderv2.py
+++ timesheetReminderv2.py
@@ -20,10 +20,11 @@ def send_timesheet_email(tracker: str, sheetname: str, contact_list: str, requir
         leads = DisciplineLead(first_name=lead[0], last_name=lead[1], email=lead[2], discipline=lead[3])
         if leads.first_name in required_contacts:
             outcome, alert = TimesheetEmail.send_timesheet_reminder(first_name= leads.first_name, discipline=leads.discipline, email_to= leads.email, cc_contacts=cc_contacts)
-            print(outcome, alert)
             if outcome is True:
-                ExcelTracker(tracker, sheetname).add_to_tracker(first_name= leads.first_name, last_name=leads.last_name, discipline=leads.discipline, email= leads.email, comment=sheetname + ' ' + alert, cc_contacts=cc_contacts)
                 print('Timesheet email alert activated - message sent')
+                ExcelTracker(tracker, sheetname).add_to_tracker(first_name= leads.first_name, last_name=leads.last_name, discipline=leads.discipline, email= leads.email, comment=sheetname + ' ' + alert, cc_contacts=cc_contacts)
+            else:
+                print(outcome)
 
 if __name__ == "__main__":
     send_timesheet_email(filenames['email_tracker'], filenames['sheetname'], filenames['contacts_list'], contacts['mailTo'], contacts['mailCC'])
