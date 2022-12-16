b1d01a3d816f9868132d8ee9e8fc2809d66466a1
Elizabeth <a.adejumo13@gmail.com>
2022-12-16 16:53:00 +0000

    remove file

--- Batch files/invoiceReminder.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\ukaea001\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\invoiceReminderv3.py"
-pause
--- Batch files/timesheetReminders.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\ukaea001\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\ukaea001\Documents\PythonPrograms\PLMB\Reminders\timesheetReminderv3.py"
-pause
